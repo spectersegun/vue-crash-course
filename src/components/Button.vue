e597c562629ed23e7510cda4f8532875df409f13
spectersegun <segunomojolowo@gmail.com>
2023-08-27 06:12:41 +0100

    first commit

---
+++ src/components/Button.vue
@@ -0,0 +1,28 @@
+<template>
+  <button
+    @click="$emit('btn-click')"
+    :style="{ background: color }"
+    class="btn"
+  >
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    text: {
+      type: String,
+    },
+    color: {
+      type: String,
+    },
+  },
+  methods: {
+    onClick() {
+      this.$emit('toggle-add-task');
+    },
+  },
+};
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,47 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button
+      v-show="homePage"
+      :text="showAddTask ? 'Close' : 'Add Task'"
+      :color="showAddTask ? 'red' : 'green'"
+      @btn-click="$emit('toggle-add-task')"
+    />
+    <!-- <Button text="Update Task" color="blue" />
+    <Button text="Delete Task" color="red" /> -->
+  </header>
+</template>
+
+<script>
+import Button from './Button.vue';
+export default {
+  name: 'Header',
+  props: {
+    title: {
+      type: String,
+    },
+    showAddTask: Boolean,
+  },
+  components: {
+    Button,
+  },
+  computed: {
+    homePage() {
+      if (this.$route.path === '/') {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 20px;
+}
+</style>
